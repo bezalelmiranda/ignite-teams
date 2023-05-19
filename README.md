ae3ab04111bc7377c82503cbc6581dde53ff6db2
bezalelmiranda <begmr206@gmail.com>
2023-05-19 17:21:59 -0300

    First commit Ignite Teams

---
+++ README.md
@@ -0,0 +1,2 @@
+npm install --save-dev babel-plugin-module-resolver  
+npm install --save-dev @types/styled-components @types/styled-components-react-native
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Title } from './styles';
+
+export default function Groups() {
+  return (
+    <Container>
+      <Title>Open up App.tsx to start working on your app!</Title>
+    </Container>
+  );
+}
